11ec8457ae4b35326407b594781e3635b51d25cb
Steve Pepple <stephen.pepple@gmail.com>
2021-11-16 18:33:56 +0000

    Add pretty printing

--- join_schema_places.py
+++ join_schema_places.py
@@ -2,6 +2,10 @@
 import pandas as pd
 import glob
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
 # %%
 print('Import select files from the Common Crawl project and join them into a single Pandas Databframe for analysis.')
 
@@ -54,5 +58,17 @@ columns = [
 df_to_dict = combined_frame[columns].to_dict(orient='records')
 # %%
 print('This many places ', len(df_to_dict))
-print(df_to_dict[0])
+pp.pprint(df_to_dict[0])
+
 # %%
+flat_dict = []
+for item in df_to_dict:
+  #print(item['aggregaterating'], type(item['aggregaterating']) is dict)
+  if item['aggregaterating'] and type(item['aggregaterating']) is dict:
+    item['ratingvalue'] = item['aggregaterating']['ratingvalue'].replace('E0', '')
+    item['reviewcount'] = float(item['aggregaterating']['reviewcount'])
+
+  flat_dict.append(item)
+
+new_df = pd.DataFrame.from_dict(flat_dict)
+new_df_sorted = new_df.sort_values(by=['reviewcount'], ascending=False)
