258b63063392302296ccf2831b2788ca3f51d5ac
Steve Pepple <stephen.pepple@gmail.com>
2021-11-23 00:20:58 +0000

    Add city filters

--- join_schema_places.py
+++ join_schema_places.py
@@ -65,10 +65,46 @@ flat_dict = []
 for item in df_to_dict:
   #print(item['aggregaterating'], type(item['aggregaterating']) is dict)
   if item['aggregaterating'] and type(item['aggregaterating']) is dict:
-    item['ratingvalue'] = item['aggregaterating']['ratingvalue'].replace('E0', '')
-    item['reviewcount'] = float(item['aggregaterating']['reviewcount'])
+    item['rating'] = item['aggregaterating']['ratingvalue'].replace('E0', '')
+
+    if 'reviewcount' in item['aggregaterating']:
+      item['review_count'] = float(item['aggregaterating']['reviewcount'])
+
+  if item['address'] and type(item['address']) is dict and 'addresslocality' in item['address']:
+    item['city'] = item['address']['addresslocality']
+
+  if item['geo'] and type(item['geo']) is dict:
+    try:
+      item['latitude'] = item['geo']['latitude']
+      item['longitude'] = item['geo']['longitude']
 
   flat_dict.append(item)
 
 new_df = pd.DataFrame.from_dict(flat_dict)
 new_df_sorted = new_df.sort_values(by=['reviewcount'], ascending=False)
+# %%
+new_df.city.head(1000)
+# %%
+# Get a unique dict of all cities
+unique_cities = new_df.city.unique()
+# %%
+for x in unique_cities:
+  print(x)
+# %%
+df_with_cities = new_df[new_df['city'].notnull()]
+df_with_cities.head()
+
+# %%
+los_angeles = df_with_cities[df_with_cities['city'].str.contains("Los Angeles")]
+
+los_angeles_sorted = los_angeles.sort_values(by=['review_count'], ascending=False)
+
+los_angeles_sorted.count()
+
+export_columns = ['name', 'review_count', 'rating', 'longitude', 'latitude']
+los_angeles_sorted.to_csv('./data/los_angeles_sorted.csv')
+
+# %%
+fort_lauderdale = df_with_cities[df_with_cities['city'].str.contains("Fort Lauderdale")]
+fort_lauderdale.count()
+# %%
